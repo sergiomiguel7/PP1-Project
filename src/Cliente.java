e9a296c3a0f542a82fa4223bb5bc03d4bc5e68b2
Sergio <sergiompc12@gmail.com>
2019-10-08 11:40:46 +0100

    Adicionados getters e setters

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,13 +1,70 @@
+import java.awt.geom.Point2D;
+
 public class Cliente
 {
     private String email, nome,password,morada,dataN;
-    private int posX, posY;
+    private int x, y;
 
-    public int getPosX() {
-        return posX;
+    //construtores
+    public Cliente()
+    {
+        this.dataN= " ";
+        this.email=" ";
+        this.morada=" ";
+        this.nome=" ";
+        this.password=" ";
+        this.x=0;
+        this.y=0;
+    }
+    public Cliente (String email, String nome , String password, String morada, String dataN, int x , int y)
+    {
+        this.email=email;
+        this.nome=nome;
+        this.password=password;
+        this.morada=morada;
+        this.dataN=dataN;
+        this.x=x;
+        this.y=y;
     }
 
-    public int getPosY() {
-        return posY;
-    }
+
+    //getters
+    public String getDataN() { return dataN; }
+
+    public String getEmail() { return email; }
+
+    public String getMorada() { return morada; }
+
+    public String getNome() { return nome; }
+
+    public String getPassword() { return password; }
+
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+    //setters
+
+    public void setDataN(String dataN) { this.dataN = dataN; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public void setMorada(String morada) { this.morada = morada; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public void setY(int y) { this.y = y; }
+
+    public void setX(int x) { this.x = x; }
+
+    //metodos
+
+
+
+
+
+
+
+
 }
