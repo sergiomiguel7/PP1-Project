7d774bbf7c69b85abb1cbe346afc3ae4670d8218
Sergio <sergiompc12@gmail.com>
2019-10-08 23:50:32 +0100

    Classe Cliente e Historico

---
+++ src/Cliente.java
@@ -0,0 +1,5 @@
+public class Cliente extends Ator{
+    private Historico historicoC;
+
+
+}
---
+++ src/Historico.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Historico {
+    private ArrayList<String> adefinir = new ArrayList<String>();
+
+
+}
