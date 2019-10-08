6481ccb5cad3c4c98176f3660e9680a69fe48857
Sergio <sergiompc12@gmail.com>
2019-10-08 11:12:08 +0100

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente
+{
+    private String email, nome,password,morada,dataN;
+    private int posX, posY;
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+}
